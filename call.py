2b0451e7cb766b38b5b0ed6b54d00267a70bccba
jlonghku <43452098+jlonghku@users.noreply.github.com>
2020-09-16 15:05:20 +0800

    Add files via upload

--- call.py
+++ call.py
@@ -10,57 +10,68 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
     super(MyMainWindow, self).__init__(parent)
     self.setupUi(self)
     self.pushButton.clicked.connect(self.cal)
-    self.pushButton_2.clicked.connect(self.app)
+    self.pushButton_2.clicked.connect(self.tem)
 
-  def app(self):
+# set template of different constructed wetlands
+  def tem(self):
     cb0=[self.comboBox_1.currentIndex(),self.comboBox_2.currentIndex(),self.comboBox_3.currentIndex(),self.comboBox_4.currentIndex()]
     if cb0[0]==0:
       setcb0(self,[1,0])
       setcb(self,[1,0,0,0])
-      setsp(self,[1,0,0,0],[1,1,1,0,0,0],[1,0,1,0]) 
+      setsp(self,[1,1,1,1],[1,1,1,0,0,0]) 
     elif cb0[0]==1:
       setcb0(self,[0,1])
       setcb(self,[0,1,0,0])
-      setsp(self,[1,0,0,0],[1,1,0,1,0,0],[1,0,1,0])
+      setsp(self,[1,1,1,1],[1,1,1,0,1,0])
     elif cb0[0]==2:
-      setcb0(self,[1,2])
-      setcb(self,[1,0,0,1])
-      setsp(self,[1,0,0,0],[1,1,1,0,0,0],[0,0,1,1])
+      setcb0(self,[0,1])
+      setcb(self,[0,1,0,0])
+      setsp(self,[1,1,1,1],[0,0,1,0,0,1])
     elif cb0[0]==3:
-      setcb0(self,[1,2])
+      setcb0(self,[1,0])
       setcb(self,[1,0,0,1])
-      setsp(self,[1,1,0,0],[1,1,1,0,0,0],[0,0,1,1])
+      setsp(self,[1,1,1,1],[1,1,0,0,1,0])
     elif cb0[0]==4:
-      setcb0(self,[1,2])
+      setcb0(self,[2,1])
       setcb(self,[1,0,0,1])
-      setsp(self,[1,0,0,0],[1,1,1,0,0,0],[0,0,1,1])
+      setsp(self,[1,1,1,1],[1,1,1,1,0,0])
     elif cb0[0]==5:
+      setcb0(self,[1,1])
+      setcb(self,[1,0,0,1])
+      setsp(self,[1,1,1,1],[0,0,1,0,0,1])
+    elif cb0[0]==56:
+      setcb0(self,[2,1])
+      setcb(self,[1,0,0,1])
+      setsp(self,[1,1,1,1],[1,1,0,1,0,0])
+    elif cb0[0]==7:
       setcb0(self,[0,0])
       setcb(self,[0,0,0,0])
-      setsp(self,[0,0,0,0],[0,0,0,0,0,0],[0,0,0,0])
+      setsp(self,[1,1,1,1],[0,0,0,0,0,0])
 
   def cal(self):
     cb0=[self.comboBox_1.currentIndex(),self.comboBox_2.currentIndex(),self.comboBox_3.currentIndex(),self.comboBox_4.currentIndex()]
     cb=[self.checkBox.isChecked(),self.checkBox_1.isChecked(),self.checkBox_2.isChecked(),self.checkBox_3.isChecked()]
     sp1=[self.spinBox_11.value(),self.spinBox_12.value(),self.spinBox_13.value(),self.spinBox_14.value()]
     sp2=[self.spinBox_21.value(),self.spinBox_22.value(),self.spinBox_23.value(),self.spinBox_24.value(),self.spinBox_25.value(),self.spinBox_26.value()]
-    sp3=[self.spinBox_31.value(),self.spinBox_32.value(),self.spinBox_33.value(),self.spinBox_34.value()]#if  self.lineEdit.text() !="1":
     T=[[2,0,1,-1],[-2,0,-1,1],[1,0,2,1],[2,1,1,-1],[0,2,0,0],[0,0,2,1],[0,0,0,2],[0,2,0,0],[0,0,0,-2],[0,2,0,0]]
     SP1=numpy.array(T[cb0[3]])+numpy.array(sp1)
     if cb0[1]==0 :
-      SP1=SP1*2
+      SP1=SP1+numpy.sign(SP1)
     if cb0[2]!=0:
-      SP1[2]=0
-    if cb[0]==0&cb[3]==0&(cb0[3]==1|cb0[3]==2|cb0[3]==3|cb0[3]==4):
+      sp1[2]=0
+    if ((cb[0]==0&cb[3]==0)&((cb0[3]==0)|(cb0[3]==1)|(cb0[3]==2)|(cb0[3]==3))):
       SP1=[0,0,0,0]
+      SP2=[0,0,0,0,0,0]
+      setlb1(self,SP1)
+      setlb2(self,SP2)
+    else:
+      SP1=SP1*sp1
+      setlb1(self,SP1)
+      T2=[[-1,1,1,1,1,1],[1,-1,-1,1,-1,1],[-1,1,1,-1,1,-1],[1,-1,-1,-1,-1,-1]]
+      SP2=SP1.dot(numpy.array(T2))
+      SP2=SP2*numpy.sign(sp2)
+      setlb2(self,SP2)
 
-    setlb1(self,SP1)
-    T2=[[1,1,1,1,1,1],[-1,1,1,1,-1,1],[0,0,0,-1,0,0],[-1,-1,-1,-1,-1,-1]]
-    SP2=SP1.dot(numpy.array(T2))
-    setlb2(self,SP2)
-    T3=[[1,1,1,1],[-1,-1,-1,-1],[-1,1,-1,1],[0,-1,0,0]]
-    SP3=SP1.dot(numpy.array(T3))
-    setlb3(self,SP3)
 
 def setlb2(self,a) :
   self.label21.setText(strrs(a[0]))
@@ -70,35 +81,24 @@ def setlb2(self,a) :
   self.label25.setText(strrs(a[4]))
   self.label26.setText(strrs(a[5]))
   
-def setlb3(self,a) :
-  self.label31.setText(strr(a[0]))
-  self.label32.setText(strr(a[1]))
-  self.label33.setText(strr(a[2]))
-  self.label34.setText(strr(a[3]))
-  
+
 def strrs(a):
   if a==0:
-    return "no major change"
-  elif a==1:
-    return "increase"
-  elif a==2:
-    return "increase significantly"
-  elif a>=3:
-    return "increase extremely"
-  elif a==-1:
-    return "decrease"
-  elif a==-2:
-    return "decrease significantly"
-  elif a<=-3:
-    return "decrease extremely"
+    return "-"
+  elif 0<a<=1:
+    return "Increase slightly"
+  elif 1<a<=2:
+    return "Increase"
+  elif a>2:
+    return "Increase significantly"
+  elif -1<=a<0:
+    return "Decrease slightly"
+  elif-2<=a<-1:
+    return "Decrease "
+  elif a<=-2:
+    return "Decrease significantly"
 
-def strr(a):
-  if a==0:
-    return "no major influence"
-  elif a>=1:
-    return "beneficial"
-  elif a<=-1:
-    return "harmful"
+
   
 
 def setlb1(self,a):
@@ -118,7 +118,7 @@ def setcb0(self,a):
   self.comboBox_3.setCurrentIndex(a[1])
 
 
-def setsp(self,a,b,c):
+def setsp(self,a,b):
   self.spinBox_11.setValue(a[0])
   self.spinBox_12.setValue(a[1])
   self.spinBox_13.setValue(a[2])
@@ -129,10 +129,7 @@ def setsp(self,a,b,c):
   self.spinBox_24.setValue(b[3])
   self.spinBox_25.setValue(b[4])
   self.spinBox_26.setValue(b[5])
-  self.spinBox_31.setValue(c[0])
-  self.spinBox_32.setValue(c[1])
-  self.spinBox_33.setValue(c[2])
-  self.spinBox_34.setValue(c[3])
+
 
    
     
